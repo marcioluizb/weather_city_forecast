2a32ace46710cd4f7199c54b421a548b556c64c8
marcio <marcio_luiz_b@yahoo.com.br>
2020-11-05 12:04:38 -0300

    insert tests in the weather class

--- lib/weather_city_forecast.rb
+++ lib/weather_city_forecast.rb
@@ -2,5 +2,6 @@ require "weather_city_forecast/version"
 
 module WeatherCityForecast
   class Error < StandardError; end
-  # Your code goes here...
+  class Weather
+  end
 end
--- spec/weather_city_forecast_spec.rb
+++ spec/weather_city_forecast_spec.rb
@@ -3,7 +3,23 @@ RSpec.describe WeatherCityForecast do
     expect(WeatherCityForecast::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe WeatherCityForecast::Weather do
+    it "check create instance" do
+      weather = WeatherCityForecast::Weather.new(ENV["APPID"])
+      expect(weather).to respond_to(:class)
+    end
+    it "check return weather by city return hash" do
+      weather = WeatherCityForecast::Weather.new(ENV["APPID"])
+      expect(weather.weather_by_city('santos')).to respond_to(:hash)
+    end
+    it "check return argument error if not city" do
+      weather = WeatherCityForecast::Weather.new(ENV["APPID"])
+      expect(weather.weather_by_city()).to eq('City is required')
+    end
+    it "check return not found if the city not found" do
+      weather = WeatherCityForecast::Weather.new(ENV["APPID"])
+      expect(weather.weather_by_city('saao paulo')).to eq('404 Not Found')
+    end
   end
+  
 end
